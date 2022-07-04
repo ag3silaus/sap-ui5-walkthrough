192d5ee6716738303c90f73bcd4eac18b05e0865
Burak Yilmaz <58527608+ag3silaus@users.noreply.github.com>
2022-07-04 11:46:36 +0200

    master

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,23 +1,9 @@
 sap.ui.define(
   [
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
+    "sap/ui/core/mvc/Controller"
   ],
-  function (Controller, MessageToast, JSONModel, ResourceModel) {
+  function (Controller) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.App", {
-      onShowHello: function () {
-        // read message from i18n model
-        var oBundle = this.getView().getModel("i18n").getResourceBundle();
-        var sRecipient = this.getView()
-          .getModel()
-          .getProperty("/recipient/name");
-        var sMessage = oBundle.getText("helloMsg", [sRecipient]);
-        //Show a native vanillaJS alert
-        MessageToast.show(sMessage);
-      },
     });
-  }
-);
+  });
